152aa5972016fe07c499f0ef16982baa95858695
Diana Garcia <dagarcia88@miners.utep.edu>
2022-08-02 17:30:54 -0600

    updated led.c

--- project/src/led.c
+++ project/src/led.c
@@ -21,7 +21,6 @@ void led_update()
 {
   if (led_changed) {
     char ledFlags = redVal[red_on] | greenVal[green_on];
-    
     P1OUT &= (0xff^LEDS) | ledFlags;
     P1OUT |= ledFlags;
     led_change = 0;
@@ -49,8 +48,18 @@ void green_on(int on)
     } else {
       P1OUT &= ~LED_GREEN;
       green_on = 0;
+  }
 }
-
+ 
+void leds_on(int on)
+  {
+    if(on)
+      {
+	P1OUT |= LEDS;
+      }else
+      P1OUT &= ~LEDS;
+  }
+  
 void leds_off()
 {
   red_on = 0;
@@ -60,3 +69,20 @@ void leds_off()
   
 }
       
+ void swap_leds(int on)
+ {
+   switch(on)
+     {
+     case 0:
+       red_on(1);
+       green_on(0);
+       break;
+     case 1:
+       red_on(0);
+       green_on(1);
+       break;
+     default:
+       leds_on(0);
+       break;
+     }
+ }
