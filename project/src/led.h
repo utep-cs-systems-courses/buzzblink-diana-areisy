88d3cbf04646a2a5e55c75d626e53630aa9c83e0
Diana Garcia <dagarcia88@miners.utep.edu>
2022-08-02 17:36:51 -0600

    changes made

--- project/src/led.h
+++ project/src/led.h
@@ -6,7 +6,6 @@
 #define LED_RED BIT6        // P1.6
 #define LEDS (BIT0 | BIT6)
 
-extern unsigned char red_on, green_on;
 extern unsigned char led_changed;
 
 void led_init();            //Initialize LEDs
